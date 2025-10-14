bdaed2f0aac684e49117b4ecc6b16a9e5f1cc1dd
Marouane Bouglace <bouglacemarouane@gmail.com>
2025-10-14 21:45:24 +0100

    Update and rename tp4Exo2.kt to Exo2.kt

---
+++ Tps_Correction/src/tp4/Exo2.kt
@@ -0,0 +1,81 @@
+fun main() {
+
+   val oiseau = Oiseau("bijou")
+   val chien = Chien("german dog")
+
+
+   oiseau.parler()
+   oiseau.nom = ""
+   chien.parler()
+
+}
+
+
+interface Animal {
+   var nom: String
+
+   fun parler() {}
+   fun seDéplacer(){}
+}
+
+interface Volant {
+   var nom: String
+
+   fun voler() {}
+}
+
+
+
+class Oiseau (var genre : String) : Animal, Volant{
+   override fun seDéplacer() {
+       println("Genre : $genre")
+       println("--------------------------")
+   }
+
+   override var nom: String
+       get() = nom
+       set(value) {
+           nom = value
+       }
+
+   override fun parler() {
+       println("L'oiseau chante")
+   }
+   override fun voler() {
+
+   }
+}
+
+class Chien (var genre : String) : Animal{
+   override fun seDéplacer() {
+       println("Genre : $genre")
+   }
+   override fun parler() {
+       println("Le chien aboie.")
+   }
+
+   override var nom: String
+       get() = nom
+       set(value) {
+           nom = value
+       }
+
+}
+
+class Poisson (var genre : String) : Animal{
+   override fun seDéplacer() {
+       println("Genre : $genre")
+   }
+   override fun parler() {
+       println("Le poisson ne fait pas de bruit.")
+   }
+
+   override var nom: String
+       get() = nom
+       set(value) {
+           nom = value
+       }
+
+}
+
+
