ff33962d242aa72f8039d3b93612ca081edc8cea
Marouane Bouglace <bouglacemarouane@gmail.com>
2025-10-14 21:44:59 +0100

    Update and rename tp4Exo1.kt to Exo1.kt

---
+++ Tps_Correction/src/tp4/Exo1.kt
@@ -0,0 +1,61 @@
+fun main() {
+
+   val car = Car("BMW", "Red")
+   val bike = Bike("green")
+
+
+   car.start()
+   car.stop()
+   bike.start()
+   bike.stop()
+
+}
+
+
+interface Vehicle {
+   fun start() {
+
+   }
+   fun stop() {
+
+   }
+}
+
+open class SuperCar (var nom : String){
+   open fun afficher() {
+       println("Nom : $nom")
+       println("--------------------------")
+   }
+}
+
+class Car(nom: String,var couleur : String) : Vehicle,SuperCar(nom) {
+   override fun start() {
+       println("Couleur : $couleur")
+       println("--------------------------")
+   }
+       override fun stop() {
+       println("Couleur : $couleur")
+       println("--------------------------")
+   }
+
+   override fun afficher() {
+       super.afficher()
+       println("Nom : $nom")
+       println("--------------------------")
+   }
+
+
+}
+
+class Bike(var couleur: String) : Vehicle {
+   override fun start() {
+       println("Couleur : $couleur")
+       println("--------------------------")
+   }
+       override fun stop() {
+       println("Couleur : $couleur")
+       println("--------------------------")
+   }
+
+}
+
